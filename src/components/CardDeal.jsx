432a7394543b653e2290b91b57444a9800eebb78
Irwing <86335744+Irwingduran@users.noreply.github.com>
2024-02-15 00:06:08 -0600

    Add files via upload

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
-const CardDeal = () => {
-    return (
-        <div>CardDeal</div>
-    )
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Fraud-proof <br className="sm:block hidden" /> Authentication.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        We verify personal data such as Identity, Title Deeds, Municipal Operating Licenses, Personal and Business Addresses,
+        Reputation for real customers, local credit history, and many other integrations.
+        <br />
+        We do all of this with zK technology, iA, and human verification.
+      </p>
 
-export default CardDeal
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
