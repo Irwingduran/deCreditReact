8d10a49f551614b7d6d1217c4cc63a20f6c48ef2
Irwing <86335744+Irwingduran@users.noreply.github.com>
2024-02-27 23:55:37 -0600

    Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,11 +7,13 @@ const Hero = () => {
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+          <a href="https://explorer.gitcoin.co/#/round/42161/0x0c12752d28a68fd3dc791826b3405b8803aa8a52/0x0c12752d28a68fd3dc791826b3405b8803aa8a52-0">
           <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
           <p className={`${styles.paragraph} ml-2`}>
             <span className="text-white">We are</span> a public good.{" "}
-            <span className="text-white">Fund us</span> <a href="https://explorer.gitcoin.co/#/round/42161/0x0c12752d28a68fd3dc791826b3405b8803aa8a52/0x0c12752d28a68fd3dc791826b3405b8803aa8a52-0">here</a>
-         . </p>
+            <span className="text-white">Fund us</span> here.
+          </p>
+            </a>
         </div>
 
         <div className="flex flex-row justify-between items-center w-full">
